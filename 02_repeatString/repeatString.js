5312595006c910ed31a5e9bbc4e65654d2d00810
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-10-22 16:21:27 -0700

    Completed repeatedString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatedVal, repetitions) {
+    if (repetitions < 0) {
+        return 'ERROR';
+    }
+    let solution = '';
+    for (let i = 0; i < repetitions; i++) {
+        solution += repeatedVal;
+    }
+    return solution;
 };
 
 // Do not edit below this line
