19dbebc270f4c5e094db64fb49903ad067508a51
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-10-22 17:38:55 -0700

    Completed removeFromArray.js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    // New array to store non-args values
+    let newArr = [];
+    // Traverse user given array to push non-arg values to new array
+    arr.forEach((item) => {
+        if(!args.includes(item)) {
+            newArr.push(item);
+        }
+    })
+    return newArr;
 };
 
 // Do not edit below this line
