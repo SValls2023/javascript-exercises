5917825b126d3dbf44b935a2069ab1114cd18858
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-10-22 18:23:12 -0700

    Completed tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  const celsius = 5*(Number(degreesF) - 32)/9;
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  const fahrenheit = Number(degreesC)*(9/5)+32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
