edc425b30542d5935196b4383142ee22cd256ba6
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-10-22 17:54:27 -0700

    Completed sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof(start) !== "number" || typeof(end) !== "number") {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (start > end) {
+        let temp = start; 
+        start = end;
+        end = temp;
+    }
+    let sum = 0;
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
