c527ee89104195eda29aa3c39197d302751516b9
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-12-03 15:00:39 -0800

    "Complete fibonacci sequence exercise"

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (Number(n) < 0) return "OOPS";
+
+    let firstPrevious = 1;
+    let secondPrevious = 0;
+
+    for (let i = 0; i < Number(n - 1); i++) {
+        let currNum = firstPrevious + secondPrevious;
+        secondPrevious = firstPrevious;
+        firstPrevious = currNum;
+    }
+
+    return firstPrevious;
 
 };
 
