1ba90c7674ae2dcf8ede921e320dd7f9f65321d4
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-12-03 14:18:01 -0800

    "Complete palindromes exercise"

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // str = str.toLowerCase();
+    // const newStr = str.match(/[a-z0-9]/g)
 
+    // let right = newStr.length - 1;
+
+    // for (let i = 0; i <= newStr.length / 2; i++) {
+    //     if(newStr[i] != newStr[right]) {
+    //         return false;
+    //     }
+    //     right--;
+    // }
+    
+    // return true;
+
+    const newStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return newStr.split("").reverse().join("") === newStr;
 };
 
 // Do not edit below this line
