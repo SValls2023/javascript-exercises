13fc47eaddc7fa2d7b3d7789a5aa5299a33e4ca3
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-12-03 15:51:54 -0800

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b) {
+        let lastPerson = 0;
+        let nextPerson = 0;
 
+        if (Object.hasOwn(a, 'yearOfDeath')) {
+            lastPerson = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            lastPerson = (new Date().getFullYear()) - a.yearOfBirth;
+        }
+
+        if (Object.hasOwn(b, 'yearOfDeath')) {
+            nextPerson = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            nextPerson = (new Date().getFullYear()) - b.yearOfBirth;
+        }
+
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
